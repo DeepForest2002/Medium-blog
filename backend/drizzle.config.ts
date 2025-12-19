b9a855414a2c06a33f8c1f988e862e7ba6d8988b
Sayan Pramanik <pramaniksayan123@gmail.com>
2025-12-20 01:53:22 +0530

    Backend modified

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 import "dotenv";
 import "pg";
 export default defineConfig({
-  schema: "./db/schema.ts",
+  schema: "./src/db/schema.ts",
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
