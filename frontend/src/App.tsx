548005e1bd02669763af9348100888dfb35d7d92
Sayan Pramanik <pramaniksayan123@gmail.com>
2026-01-07 01:37:17 +0530

    frontend added

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { Signin } from "./pages/Signin";
+import { Signup } from "./pages/Signup";
+import { Pages } from "./pages/Pages";
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/blog/:id" element={<Pages />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,3 @@
+export const Signup = () => {
+  return <div>Hello Signup page</div>;
+};
