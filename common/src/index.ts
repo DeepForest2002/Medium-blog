88ae7422b63ab0e96fcffbb877601d8a890e7898
Sayan Pramanik <pramaniksayan123@gmail.com>
2026-01-15 23:03:12 +0530

    Some Changes in the common index.ts file

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,30 +1,30 @@
-import zod, { email } from "zod";
+  import zod, { email } from "zod";
 
-export const signupInput = zod.object({
-  name: zod.string(),
-  email: zod.string().email(),
-  password: zod.string().min(8),
-});
+  export const signupInput = zod.object({
+    name: zod.string(),
+    email: zod.string().email(),
+    password: zod.string().min(8),
+  });
 
-export type signupType = zod.infer<typeof signupInput>;
+  export type signupType = zod.infer<typeof signupInput>;
 
-export const signinInput = zod.object({
-  email: zod.string().email(),
-  password: zod.string().min(8),
-});
+  export const signinInput = zod.object({
+    email: zod.string().email(),
+    password: zod.string().min(8),
+  });
 
-export type signinType = zod.infer<typeof signinInput>;
+  export type signinType = zod.infer<typeof signinInput>;
 
-export const createPostInput = zod.object({
-  title: zod.string(),
-  content: zod.string(),
-  authorId: zod.number(),
-});
-export type createPostType = zod.infer<typeof createPostInput>;
+  export const createPostInput = zod.object({
+    title: zod.string(),
+    content: zod.string(),
+    authorId: zod.number(),
+  });
+  export type createPostType = zod.infer<typeof createPostInput>;
 
-export const updatePostInput = zod.object({
-  id: zod.number(),
-  title: zod.string().optional(),
-  content: zod.string().optional(),
-  authorId: zod.number(),
-});
+  export const updatePostInput = zod.object({
+    id: zod.number(),
+    title: zod.string().optional(),
+    content: zod.string().optional(),
+    authorId: zod.number(),
+  });
